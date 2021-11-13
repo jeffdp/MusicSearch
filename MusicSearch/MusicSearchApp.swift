1fffdef3af683850e6963dd8d01d72dcc6dcb7d8
Jeff Porter <jeff.porter@acm.org>
2021-11-13 12:02:30 -0800

    Initial Commit

---
+++ MusicSearch/MusicSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicSearchApp.swift
+//  MusicSearch
+//
+//  Created by Jeffrey Porter on 11/13/21.
+//
+
+import SwiftUI
+
+@main
+struct MusicSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
